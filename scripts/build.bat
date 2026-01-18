276c16b95b7bcc067af190211f144a32fc24aed1
Diep Gia Huy <Huycan1211@gmail.com>
2026-01-18 16:05:54 +0000

    - Update build script and build command in gh action - Update csproj (publish only 1 file)

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,1 +1,3 @@
-dotnet publish "../src/Agent/OurDLP.Core/OurDLP.Core.csproj" -c Release 
+set SRC_CSPROJ=%1
+set SW_OUT_PATH=%2
+dotnet publish %SRC_CSPROJ% -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -o %SW_OUT_PATH%
